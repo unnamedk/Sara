5bfdd25b21b3d32bd032b462b0b97615279b2d12
unnamedk <idkaname_2@outlook.com>
2022-02-02 23:24:30 -0300

    add logging and bot dependencies

--- Sara/Program.cs
+++ Sara/Program.cs
@@ -1,11 +1,33 @@
-﻿using System;
+﻿using NLog;
+using System;
+using Discord;
+using Discord.WebSocket;
 
 namespace Sara
 {
     public class Program
     {
+        private static readonly Logger _logger = LogManager.GetCurrentClassLogger();
+        private static DiscordSocketClient _client;
+
         public static async Task Main(string[] args)
         {
+            // setup bot client
+            _client = new DiscordSocketClient();
+            _client.Log += Log;
+
+            // try to log into Discord
+            var botToken = Environment.GetEnvironmentVariable("DISCORD_TOKEN");
+            await _client.LoginAsync(TokenType.Bot, botToken);
+            await _client.StartAsync();
+
+            // block task until the program is closed
+            await Task.Delay(-1);
+        }
+
+        private static async Task Log(LogMessage msg)
+        {
+            _logger.Info(msg);
         }
     }
 }
